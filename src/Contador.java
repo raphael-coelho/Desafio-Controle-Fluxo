ce72862660d71a5e7e54f1e07cdf061cedd03f26
Raphael coelho <raphaelcostay@gmail.com>
2024-05-28 15:39:08 -0300

    segundo  commit desafio

--- src/Contador.java
+++ src/Contador.java
@@ -9,6 +9,7 @@ public class Contador {
         Scanner sc = new Scanner(System.in);    
         int numero1;
         int numero2;
+        
         System.out.println("**************************");
         System.out.println("BEM VINDO AO SEU CONTADOR!");
         System.out.println("**************************");
@@ -21,21 +22,20 @@ public class Contador {
         System.out.println("--- AGUARDE UM MOMENTO ---");
         System.out.println("--- ESTAMOS FAZENDO A CONTAGEM ---");
 
-        try (contar numero1 +numero2) {
-        catch ParametrosInvalidosException{
+        try {
+            contar (numero1,numero2);
+        } catch(ParametrosInvalidosException e){
             System.out.println("ERRO NA CONTAGEM, TENTE NOVAMENTE!");
+            System.out.println(e.getMessage());
         }
     }
 
 
-
-
-
-
-
-
-
-
-
-    }
-}
+        public static void contar ( int numero1, int numero2) throws ParametrosInvalidosException{
+            if (numero1>numero2) {
+                throw new ParametrosInvalidosException ("O NÚMERO 2 DEVE SER MAIOR QUE O NUMERO 1.");
+            } for( int i = numero1; i <= numero2; i++ ){
+                System.out.println("NUMERO: "+ i);
+            }
+        }
+     }
